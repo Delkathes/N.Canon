703a6118e3d633bbd1396e79d972e2ff3cbb2458
Delkathes <39232759+Delkathes@users.noreply.github.com>
2019-12-03 02:57:09 +0100

    simplified next.config

--- next.config.js
+++ next.config.js
@@ -1,56 +1,21 @@
 //? IMPORT
 //! Modules
-// const path = require('path')
-// const {PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD} = require('next/constants')
-
 //! Official next plugins
 const withPlugins = require('next-compose-plugins')
-// const withSize = require('next-size')
-// const withImages = require('next-images')
-// const optimizedImages = require('next-optimized-images')
-// const withFonts = require('next-fonts')
-
-//! Absolute imports
-// const {AbsoluteImports, sharedConfig, devConfig, prodConfig} = require('./config/_extend.next')
-// function resolveHelper (config, alias, target) {
-//     config.resolve.alias[alias] = path.join(__dirname, target)
-// }
-
-//! Router
-// const {router} = require("./router")
-
 
 //! Next.js configuration
 const nextConfig = {
     //! target
     target: 'serverless',
-    //! Export PathMap
-    // exportPathMap: async () => {
-    //     return router
-    // },
-    // ...sharedConfig,
+    
     //? Webpack shared config
-    webpack: (config) => {
     // webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
-        // AbsoluteImports.map(abs => resolveHelper(config, abs.alias, abs.target)) //! absolute imports
-
+    webpack: (config) => {
         config.node = {
             fs: 'empty' //! Fixes npm packages that depend on `fs` module
         }
-        //* Loader
-        // config.module.rules.push(
-        //     {
-        //         test: /\.md$/,
-        //         use: 'frontmatter-markdown-loader'
-        //     }
-        // )
         return config
-    },
-    //? Phased configs
-    // //* Dev server
-    // [PHASE_DEVELOPMENT_SERVER]: devConfig,
-    // //* Prod build
-    // [PHASE_PRODUCTION_BUILD]: prodConfig,
+    }
 }
 
 
